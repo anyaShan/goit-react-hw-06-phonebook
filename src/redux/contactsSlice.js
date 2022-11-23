d102bffd638dcc7945c96e33f94ebdc58db9f05a
anya.shan <anya.shanaida@yahoo.com>
2022-11-23 20:39:52 +0200

    add redux components

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+
+const INITIAL_CONTACTS = [
+  { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+  { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+  { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+  { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: INITIAL_CONTACTS,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        if (state.some(contact => contact.name === action.payload)) {
+          return alert(`${action.payload.name} is already in contacts.`);
+        }
+        return [...state, action.payload];
+      },
+      prepare(name, number) {
+        return {
+          payload: {
+            id: nanoid(),
+            name,
+            number,
+          },
+        };
+      },
+    },
+
+    deleteContact(state, action) {
+      return state.filter(contact => contact.id !== action.payload);
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
